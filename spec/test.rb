782d0a30436d5ddd649e2f908f421f6bd0bac525
wajahat <wajahatakram414@gmail.com>
2021-05-28 04:37:13 +0500

    undo linter error

--- spec/test.rb
+++ spec/test.rb
@@ -1,14 +1,14 @@
 require './lib/game_logic'
 
 describe Board do
-    describe '#display_board' do
-      it 'displays the game board' do
+    describe "#display_board" do
+      it "displays the game board" do
         board = Board.new
-        expect(board.display_board).to eql(" 1 | 2 | 3 \n" \
+        expect(board.display_board).to eql(  " 1 | 2 | 3 \n" \
             "+---+---+---+\n" \
             " 4 | 5 | 6 \n" \
             "+---+---+---+\n" \
-            " 7 | 8 | 9 \n")
-        end
+            " 7 | 8 | 9 \n" )
+      end
     end
-end
+  end
