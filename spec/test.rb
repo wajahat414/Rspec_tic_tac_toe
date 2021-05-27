c6cd4c7eed386448ff7076afc638545e02dce174
wajahat <wajahatakram414@gmail.com>
2021-05-28 04:56:15 +0500

    added test case CheckWinner Function

--- spec/test.rb
+++ spec/test.rb
@@ -11,4 +11,48 @@ describe Board do
           " 7 | 8 | 9 \n")
     end
   end
+  describe '#checkwinner' do
+    it 'checks all the winning combinations' do
+      board = Board.new
+      board.board[0]='x'
+      board.board[1]='x'
+      board.board[2]='x'
+      expect(board.checkwinner).to eql(true)
+      board = Board.new
+      board.board[3]='x'
+      board.board[4]='x'
+      board.board[5]='x'
+      expect(board.checkwinner).to eql(true)
+      board = Board.new
+      board.board[6]='x'
+      board.board[7]='x'
+      board.board[8]='x'
+      expect(board.checkwinner).to eql(true)
+      board = Board.new
+      board.board[0]='x'
+      board.board[3]='x'
+      board.board[6]='x'
+      expect(board.checkwinner).to eql(true)
+      board = Board.new
+      board.board[1]='x'
+      board.board[4]='x'
+      board.board[7]='x'
+      expect(board.checkwinner).to eql(true)
+      board = Board.new
+      board.board[2]='x'
+      board.board[5]='x'
+      board.board[8]='x'
+      expect(board.checkwinner).to eql(true)
+      board = Board.new
+      board.board[0]='x'
+      board.board[4]='x'
+      board.board[8]='x'
+      expect(board.checkwinner).to eql(true)
+      board = Board.new
+      board.board[2]='x'
+      board.board[4]='x'
+      board.board[6]='x'
+      expect(board.checkwinner).to eql(true)
+    end
+  end
 end
